7da951eb1d8ff89392e2d53cc25954af96c9cc4d
alfahimbinfaruk <fmd03700@gmail.com>
2023-03-12 22:48:33 +0600

    code template added

---
+++ Template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve()
+{
+   
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
