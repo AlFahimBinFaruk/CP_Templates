1ab365111ef4b39acb6d7453637985b51577a14c
Al Fahim Bin Faruk <85015121+AlFahimBinFaruk@users.noreply.github.com>
2023-04-14 02:23:21 +0600

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 #define ll long long int
 #define nl "\n"
-#define printT(x) cout << "#case = " << x << nl
+#define printT(x,y) cout << "#For= " << x << " Value= " << y << nl
 #define print(x) cout << x << nl
-#define MAX_NUM 1e9
 #define YES cout << "YES" << nl
 #define NO cout << "NO" << nl
 #define sort(x) sort(x.begin(), x.end())
+#define reverse(x) reverse(x.begin(), x.end())
+const int maxn=1e6;
 
-void solve()
-{
+void solve(){
     
 }
 
@@ -23,13 +23,10 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-
     int t;
     cin >> t;
-    while (t--)
-    {
+    while(t--){
         solve();
     }
-
     return 0;
 }
