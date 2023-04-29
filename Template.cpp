d25752cc9fbd153c9a1a212cbb3225b76fcc1c9b
Al Fahim Bin Faruk <85015121+AlFahimBinFaruk@users.noreply.github.com>
2023-04-30 02:21:44 +0600

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -6,22 +6,25 @@ using namespace std;
 #define nl "\n"
 #define printT(x,y) cout << "#For= " << x << " Value= " << y << nl
 #define print(x) cout << x << nl
-#define printArr(x) for(auto i:x) cout << i <<  " "
 #define YES cout << "YES" << nl
 #define NO cout << "NO" << nl
+#define printArr(x) for(auto i:x) cout << i <<  " "
+#define fr(i,n) for(int i=0; i<(n); i++)
 #define sort(x) sort(x.begin(), x.end())
 #define reverse(x) reverse(x.begin(), x.end())
 #define maxe(x) *max_element(x.begin(),x.end())
 #define mine(x) *min_element(x.begin(),x.end())
 #define takeArr(x) for(auto &i:x) cin >> i
 #define all(x) (x.begin(),x.end())
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bitc(x,y)  bitset<y>(x)
 /***********************************************************
 ************************/
-ll ceil(ll x,ll y)
+int ceil(int x,int y)
 {return (x+y-1)/y;}
-const int maxn=1e6;
+const int maxn=1e9+7;
 /*-------------------------------------------------------------------------------------------------*/
-
 void solve(){
     
 }
