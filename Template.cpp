ff9afb46fffeef277bb337a38fbb1aac2657d55d
Al Fahim Bin Faruk <85015121+AlFahimBinFaruk@users.noreply.github.com>
2023-05-17 23:28:00 +0600

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -10,33 +10,19 @@ using namespace std;
 #define NO cout << "NO" << nl
 #define printArr(x) for(auto i:x) cout << i <<  " "
 #define fr(i,n) for(int i=0; i<(n); i++)
-#define sort(x) sort(x.begin(), x.end())
-#define reverse(x) reverse(x.begin(), x.end())
+#define cSort(x) sort(x.begin(), x.end())
+#define cReverse(x) reverse(x.begin(), x.end())
 #define maxe(x) *max_element(x.begin(),x.end())
 #define mine(x) *min_element(x.begin(),x.end())
 #define takeArr(x) for(auto &i:x) cin >> i
 #define all(x) (x.begin(),x.end())
 #define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
-#define tolower(x) transform(x.begin(), x.end(), x.begin(), ::tolower)
-#define toupper(x) transform(x.begin(), x.end(), x.begin(), ::toupper)
-#define find(arr,x) find(arr.begin(),arr.end(),x)
-#define count(arr,x) count(arr.begin(),arr.end(),x)
+#define toLower(x) transform(x.begin(), x.end(), x.begin(), ::tolower)
+#define toUpper(x) transform(x.begin(), x.end(), x.begin(), ::toupper)
+#define cFind(arr,x) find(arr.begin(),arr.end(),x)
+#define cCount(arr,x) count(arr.begin(),arr.end(),x)
 // make sure to sort before applying unique // else only consecutive duplicates would be removed 
 #define bitc(x,y)  bitset<y>(x)
-// Function to convert binary to decimal
-int binaryToDecimal(int n){
-    int num = n;
-    int dec_value = 0;
-    int base = 1;
-    int temp = num;
-    while (temp) {
-        int last_digit = temp % 10;
-        temp = temp / 10;
-        dec_value += last_digit * base;
-        base = base * 2;
-    }
-    return dec_value;
-}
 /***********************************************************
 ************************/
 int ceil(int x,int y){return (x+y-1)/y;}
