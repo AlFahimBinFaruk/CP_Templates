3087cbf1799c7e59d3a9b6f8c06c5c9fcbdaa603
alfahimbinfaruk <fmd03700@gmail.com>
2023-07-28 00:57:28 +0600

    tem updated

---
+++ Templates/debug.h
@@ -0,0 +1,18 @@
+#define debug(x) cout << #x<<" "; _print(x); cout << nl
+void _print(ll t) {cout << t;}
+void _print(int t) {cout << t;}
+void _print(string t) {cout << t;}
+void _print(char t) {cout << t;}
+void _print(lld t) {cout << t;}
+void _print(double t) {cout << t;}
+void _print(ull t) {cout << t;}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cout << "{"; _print(p.ff); cout << ","; _print(p.ss); cout << "}";}
+template <class T> void _print(vector <T> v) {cout << "[ "; for (T i : v) {_print(i); cout << " ";} cout << "]";}
+template <class T> void _print(set <T> v) {cout << "[ "; for (T i : v) {_print(i); cout << " ";} cout << "]";}
+template <class T> void _print(multiset <T> v) {cout << "[ "; for (T i : v) {_print(i); cout << " ";} cout << "]";}
+template <class T, class V> void _print(map <T, V> v) {cout << "[ "; for (auto i : v) {_print(i); cout << " ";} cout << "]";};
---
+++ Templates/new_temp.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double lld;
+typedef unsigned long long ull;
+
+#define nl "\n"
+#define print(x) cout<<x<<nl
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+#ifndef ONLINE_JUDGE
+#include "../debug.h"
+#else
+#define debug(x)
+#endif
+
+/** end of debug module **/
+
+/*--------------------------------------------------------
+                                    Code starts from here.
+--------------------------------------------------------------------------------------*/
+void solve(){
+   
+}
+
+signed main(){
+    fastio();
+    int t=1;
+    // cin>>t;
+    while(t--)solve();
+    return 0;
+}
