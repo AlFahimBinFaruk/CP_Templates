1793537040609884fa3b54ebcf6b8ed0441f3923
Al Fahim Bin Faruk <85015121+AlFahimBinFaruk@users.noreply.github.com>
2023-10-26 19:07:23 +0600

    Create writer.cpp

---
+++ writer.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl "\n"
+#define print(x) cout<<x<<nl
+void fastio(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+/*-------------------------------------------------------------------------------------------------*/
+
+
+
+int main(){
+    fastio();
+    
+}
