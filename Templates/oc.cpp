9a159be83193edd53681f0a47112dbdc761ccc0a
Al Fahim Bin Faruk <85015121+AlFahimBinFaruk@users.noreply.github.com>
2024-02-21 11:00:40 +0600

    Create oc.cpp

---
+++ Templates/oc.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+#define nl "\n"
+#define print(x) cout<<x<<nl
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+#ifndef ONLINE_JUDGE
+#include "../debug.h"
+#else
+#define debug(x)
+#endif
+
+
+/*--------------------------------------------------------
+                                    Code starts from here.
+--------------------------------------------------------------------------------------*/
+
+
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        
+    }
+};
+
+
+signed main(){
+    fastio();
+    
+
+    Solution SS=Solution();
+    SS.numDistinct();
+    
+}
