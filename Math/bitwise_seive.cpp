979d6d0eaad9c270985416efa272c15f67d1bef0
Al Fahim Bin Faruk <85015121+AlFahimBinFaruk@users.noreply.github.com>
2024-05-09 23:57:17 +0600

    Update bitwise_seive.cpp

--- Math/bitwise_seive.cpp
+++ Math/bitwise_seive.cpp
@@ -1,29 +1,20 @@
 //https://www.geeksforgeeks.org/bitwise-sieve/
-bool ifnotPrime(int prime[], int x) 
-{ 
-	
+bool ifnotPrime(int prime[], int x){ 
 	return (prime[x/64] & (1 << ((x >> 1) & 31))); 
 } 
 
-bool makeComposite(int prime[], int x) 
-{ 
-	
+bool makeComposite(int prime[], int x){ 
 	prime[x/64] |= (1 << ((x >> 1) & 31)); 
 } 
 
-void bitWiseSieve(int n) 
-{ 
+void bitWiseSieve(int n){ 
 	
 	int prime[n/64]; 
-
-	
 	for (int i = 3; i * i <= n; i += 2) { 
 		if (!ifnotPrime(prime, i)) 
 			for (int j = i * i, k = i << 1; j < n; j += k) 
 				makeComposite(prime, j); 
 	} 
-
-	
 	if(ifnotPrime(prime,104743)){
         cout<<"NOT prime\n";
     }else cout<<"Prime\n";
