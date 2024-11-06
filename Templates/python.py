4d207842eb8eddcab173cac6819e96c5bf0ac109
Al Fahim Bin Faruk <85015121+AlFahimBinFaruk@users.noreply.github.com>
2024-11-06 20:14:23 +0600

    Update python.py

--- Templates/python.py
+++ Templates/python.py
@@ -1,6 +1,10 @@
-import sys,math
+import sys
+sys.stdout = open('./output.txt', 'w')
+sys.stdin = open('./input.txt', 'r')
+
 input=sys.stdin.readline
 
 for T in range(int(input())):
     n=int(input())
-    a=list(map(int,input().split()))
+    a=list(map(str,input().split()))
+    print(a)
